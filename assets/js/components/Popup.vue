d6391733021740da80908000f9da1295a5f9a95f
shubham-mathur <mathurshubham@gmail.com>
2024-06-28 16:52:23 +0530

    added vue to popupjs

---
+++ assets/js/components/Popup.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper" >
+        <h1 class="title">Simple Toggle Sites</h1>
+        <div class="buttons">
+            <button type="button" class="state-off" :class="{'is-Active': !active}" @click="setActive(false)" >Off</button>
+            <button type="button" class="state-on" :class="{'is-Active': active}" @click="setActive(true)" >On</button>
+        </div>
+        <div class="sites">
+            <p>List your websites below, one per line</p>
+            <textarea v-model="list" rows="8" autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"></textarea>
+        </div>
+        <button type="button" class="state-save" @click="saveList">Save Site List</button>
+    </div>
+</template>
+
+<script>
+export default{
+    data(){
+        return {
+            active: true,
+            list: "example.com"
+        }
+    },
+    created() {
+        chrome.storage.sync.get(['toggleSitesActive','toggleSitesList'], (result)=>{
+            this.active = result.toggleSitesActive;
+            this.list = result.toggleSitesList;
+        });
+    },
+    method:{
+        setActive(active){
+            this.active = active;
+            chrome.storage.sync.set({
+                toggleSitesActive: active
+            },()=>{});
+        },
+        saveList(){
+            chrome.storage.sync.set({
+                totggleSitesList: this.list
+            },()=>{});
+        }
+    }
+}
+
+
+</script>
